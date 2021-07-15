9a080daee8888567887f02e00a7d2a200535ad39
박준영 <ib0712@naver.com>
2021-07-15 21:11:11 +0900

    백업

--- r-test/src/App.js
+++ r-test/src/App.js
@@ -10,7 +10,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Game from './game/Game';
 
 import Slider from "react-slick";
-import "./slick.css"; 
+import "./slick.css";
 import "./slick-theme.css";
 
 import imposter from './img/running_imposter_once.gif'
@@ -49,22 +49,26 @@ function FadeInSection(props) {
 }
 
 function FadeInhr(props) {
-  const [isVisible, setVisible] = React.useState(true);
-  const domRef = React.useRef(); // getElementById 처럼 특정 DOM을 선택해야하는 상황에 쓰임
+  const [isVisible, setVisible] = React.useState(true); // useState => 초기 값 true 로 정의
+  const domRef = React.useRef(); // useRef => getElementById 처럼 특정 DOM을 선택해야하는 상황에 쓰임
+  // useRef 함수는 current 속성을 가지고 있는 객체를 반환
 
   React.useEffect(() => {
     const observer = new IntersectionObserver(entries => {
       entries.forEach(entry => setVisible(entry.isIntersecting));
+      // 타겟 엘리먼트가 교차 영역에 있는 동안 true를 반환하고, 그 외의 경우 false를 반환합니다.
     });
 
+    //observer.observe(domRef.current); // 타겟 엘리먼트에 대한 IntersectionObserver를 등록할 때(관찰을 시작할 때) 사용
     const { current } = domRef;
     observer.observe(current);
 
+    //return () => observer.unobserve(domRef.current); // 타겟 엘리먼트에 대한 관찰을 멈추고 싶을 때 사용
     return () => observer.unobserve(current);
   }, []);
   return (
     <div className='blank' ref={domRef}>
-      <img src={isVisible ? props.imposter : ''}  />
+      <img src={isVisible ? props.imposter : ''} />
     </div>
   );
 }
@@ -75,35 +79,30 @@ class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time_on: true
+      time_on: true,
     };
     // 콜백에서 `this`가 작동하려면 아래와 같이 바인딩 해주어야 합니다.
     this.current_Time = this.current_Time.bind(this);
   }
-  
 
-  // reloadGif = () => {
-  //   this.setState({ loaded: '' })
-  //   setTimeout(() => {
-  //     this.setState({ loaded: this.state.gif })
-  //   }, 0)
-  // }
+
+
 
   current_Time() {
     this.setState(state => ({
       time_on: !state.time_on
     }));
-
     //console.log(this.state.time_on);
   }
+
 
   render() {
 
     // 슬라이드 설정
-    const settings = { 
+    const settings = {
       dots: true,  // carousel 밑에 지정 콘텐츠로 바로 이동할 수 있는 버튼을 뜻한다. flase 할시 사라진다.
       infinite: true, // 콘텐츠 끝까지 갔을 때 다음 콘텐츠를 처음 콘텐츠로 가져와 반복한다.
-      speed: 500, 
+      speed: 500,
       slidesToShow: 2, // 한 화면에 보이는 콘텐츠 개수
       slidesToScroll: 1, // 한번에 넘어가는 콘텐츠 수
       pauseOnHover: false, // 마우스 호버시 넘어가는거 멈춤
@@ -112,82 +111,81 @@ class Main extends Component {
     };
 
 
-
     return (
       <div className="App" >
-          <div>  
-            {/* <FadeInSection>  */}
-              <Slider {...settings}>
-                <div className="main_img_div">
-                  <img className="main_img" src={main_img1} />
-                </div>
-                <div className="main_img_div">
-                  <img className="main_img" src={main_img2} />
-                </div>
-                <div className="main_img_div">
-                  <img className="main_img" src={main_img3} />
-                  </div>
-                <div className="main_img_div">
-                  <img className="main_img" src={main_img4} />
-                </div>
-                <div className="main_img_div">
-                  <img className="main_img" src={main_img5} />
-                </div>
-                <div className="main_img_div">
-                  <img className="main_img" src={main_img6} />
-                </div>
-              </Slider>
+        <div>
+          {/* <FadeInSection>  */}
+          <Slider {...settings}>
+            <div className="main_img_div">
+              <img className="main_img" src={main_img1} />
+            </div>
+            <div className="main_img_div">
+              <img className="main_img" src={main_img2} />
+            </div>
+            <div className="main_img_div">
+              <img className="main_img" src={main_img3} />
+            </div>
+            <div className="main_img_div">
+              <img className="main_img" src={main_img4} />
+            </div>
+            <div className="main_img_div">
+              <img className="main_img" src={main_img5} />
+            </div>
+            <div className="main_img_div">
+              <img className="main_img" src={main_img6} />
+            </div>
+          </Slider>
 
-              
 
-              <div id="main_p">
-                {/* <p>Am I Doing Well?</p> */}
-                <div className="hello">
-                  <span>H&nbsp;</span>
-                  <span>E&nbsp;</span>
-                  <span>L&nbsp;</span>
-                  <span>L&nbsp;</span>
-                  <span>O&nbsp;</span>
-                  <span>!&nbsp;</span>
-                </div>
 
-                <div id="symbol">
-                  <span> Thank</span>
-                  <span> You</span>
-                  <span> For</span>
-                  <span> Visit</span>
-                  <span> Moris' </span>
-                  <span> Page</span>
-                </div>
-              </div>
-            {/* </FadeInSection> */}
+          <div id="main_p">
+            {/* <p>Am I Doing Well?</p> */}
+            <div className="hello">
+              <span>H&nbsp;</span>
+              <span>E&nbsp;</span>
+              <span>L&nbsp;</span>
+              <span>L&nbsp;</span>
+              <span>O&nbsp;</span>
+              <span>!&nbsp;</span>
+            </div>
+
+            <div id="symbol">
+              <span> Thank</span>
+              <span> You</span>
+              <span> For</span>
+              <span> Visit</span>
+              <span> Moris' </span>
+              <span> Page</span>
+            </div>
           </div>
+          {/* </FadeInSection> */}
+        </div>
 
         {/* <div className="blank">
           <img src={imposter} />
         </div> */}
-        
-        <FadeInhr imposter={imposter}/>
+
+        <FadeInhr imposter={imposter} />
 
         {/* <FadeInSection> */}
-          <Profile />
+        <Profile />
         {/* </FadeInSection> */}
 
         <FadeInhr imposter={imposter} />
 
         {/* <FadeInSection> */}
-          <Learning />
+        <Learning />
         {/* </FadeInSection> */}
 
         <FadeInhr imposter={imposter} />
 
-          {/* <button onClick={this.current_Time}>
+        {/* <button onClick={this.current_Time}>
             현재 시간 {this.state.time_on ? 'ON' : 'OFF'}
           </button>
 
           { this.state.time_on ? <Clock /> : '' } */}
 
-          {/* <Game/> */}
+        {/* <Game/> */}
 
       </div>
     );
